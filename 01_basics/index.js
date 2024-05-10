d79ab80d0abd24d53f2cecba8c71ce2325ebbb67
Roshan Kajulkar <roshankajulkar@gmail.com>
2024-05-10 10:12:37 +0000

    conversion of data types

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,1 +1,3 @@
+"use strict" // treat all code as newer version of JS
+
 console.log("Hello RK!")
---
+++ 02_conversion/index.js
@@ -0,0 +1,18 @@
+// number
+console.log(Number("33"))      // 33
+console.log(Number("33abc"))   // NaN
+console.log(Number(null))      // 0
+console.log(Number(undefined)) // NaN
+console.log(Number(true))      // 1
+console.log(Number(""))        // 0
+
+// boolean
+console.log(Boolean(0))        // false
+console.log(Boolean(""))       // false
+console.log(Boolean("roshan")) // true
+
+// string
+console.log(String(33))        // 33
+console.log(String(null))      // "null"
+console.log(String(undefined)) // "undefined"
+console.log(String(true))      // "true"
