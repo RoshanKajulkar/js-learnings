18bcfb7353cc53b35df5750f2b2be36f860c349c
Roshan Kajulkar <roshankajulkar@gmail.com>
2024-05-14 10:16:47 +0000

    hoisting

--- 09_function/index.js
+++ 09_function/index.js
@@ -5,3 +5,23 @@ const getScores = (...values) => {
 console.log(getScores());        // []
 console.log(getScores(1));       // [ 1 ]
 console.log(getScores(1, 2, 3)); // [ 1, 2, 3 ]
+
+
+// reason yet to be explored
+
+addOne(4)
+function addOne(num){
+    return num + 1
+}
+
+addTwo(4)
+const addTwo = function(num) {
+    return num + 2
+}
+
+/*
+addTwo(4)
+^
+
+ReferenceError: Cannot access 'addTwo' before initialization
+*/
